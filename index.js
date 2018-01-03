a6924690a863b0b60817e2c4356dafbda905bada
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-03 17:49:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Harris"
-var height = "73"
+var height = 74
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height)+1)
+console.log(height+1)
